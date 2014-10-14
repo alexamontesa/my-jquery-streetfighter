f27a8e89130cb97518eb5143d9a4d1450c062a16
alexamontesa <alexamontesa@hotmail.com>
2014-10-13 23:13:39 -0500

    final stuff

--- js/app.js
+++ js/app.js
@@ -1,17 +1,21 @@
 $(document).ready(function() {
+
+    var lastState = "still";
+
 	$('.ryu').mouseenter(function() {
 		$('.ryu-still').hide();
 		$('.ryu-ready').show();
+		lastState="ready";
 	});
 	$('.ryu').mouseleave(function() {
 		$('.ryu-ready').hide();
 		$('.ryu-still').show();
+		lastState="still";
 	});
 	$('.ryu').mousedown(function() {
 		playHadouken();
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
-		$('.hadouken').show();
 		$('.hadouken').finish().show()
 			.animate(
 				{'left': '300px'},
@@ -25,6 +29,24 @@ $(document).ready(function() {
 	$('.ryu').mouseup(function() {
 		$('.ryu-throwing').hide();
 		$('.ryu-ready').show();
+	});
+	$(document).keydown(function(e) {
+		if (e.keyCode == 88) {
+			$('.ryu-still').hide();
+			$('.ryu-ready').hide();
+			$('.ryu-x').show();
+		}
+	});
+	$(document).keyup(function(e) {
+		if (e.keyCode == 88) {
+			if (lastState=="still") {
+				$('.ryu-still').show();
+			}
+			else {
+				$('.ryu-ready').show();
+			}
+			$('.ryu-x').hide();
+		}
 	});
 });
 
